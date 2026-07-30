b4bfae743d89592d5c43f7018173df82c1ed47ea
liubowen36 <liubowen36@meituan.com>
2026-07-30 12:02:16 +0800

    deploy blog to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
-	base: "/",
+	site: "https://mofusigil.github.io",
+	base: "/ai-notes",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
